c29702824b8218822a4b0960109f7b7fd496c815
Pier Luigi Vidale <108409300+pierluigividale@users.noreply.github.com>
2025-02-26 17:21:05 +0000

    Add files via upload

--- Notebooks/make_lorenz_animation.py
+++ Notebooks/make_lorenz_animation.py
@@ -6,23 +6,23 @@ Adapted from http://jakevdp.github.io/blog/2013/02/16/animating-the-lorentz-syst
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
-from JSAnimation import IPython_display
-from JSAnimation import HTMLWriter
+import matplotlib
+from IPython.display import HTML  # Changed for Python 3 compatibility
+# Set the embed limit for animation (in bytes)
+matplotlib.rcParams['animation.embed_limit'] = 2**128
+from scipy import integrate
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import cnames
 
 def lorenz_animation(N_trajectories=20, rseed=1, frames=200, interval=30, rho_std=28):
     """Plot a 3D visualization of the dynamics of the Lorenz system"""
- 
-    from scipy import integrate
-    from mpl_toolkits.mplot3d import Axes3D
-    from matplotlib.colors import cnames
-
-    def lorentz_deriv((x, y, z), t0, sigma=10., beta=8./3, rho=rho_std):
-        """Compute the time-derivative of a Lorentz system."""
+    def lorentz_deriv(state, t0, sigma=10., beta=8./3, rho=rho_std):
+        """Compute the time-derivative of a Lorenz system."""
+        x, y, z = state
         return [sigma * (y - x), x * (rho - z) - y, x * y - beta * z]
 
-
     # Choose random starting points, uniformly distributed from -15 to 15
-    np.random.seed(1)
+    np.random.seed(rseed)  # using rseed for reproducibility
     x0 = -15 + 30 * np.random.random((N_trajectories, 3))
 
     # Solve for the trajectories
@@ -62,9 +62,9 @@ def lorenz_animation(N_trajectories=20, rseed=1, frames=200, interval=30, rho_st
             pt.set_3d_properties([])
         return lines + pts
 
-    # animation function.  This will be called sequentially with the frame number
+    # animation function. This will be called sequentially with the frame number
     def animate(i):
-        # we'll step two time-steps per frame.  This leads to nice results.
+        # we'll step two time-steps per frame. This leads to nice results.
         i = (2 * i) % x_t.shape[1]
 
         for line, pt, xi in zip(lines, pts, x_t):
@@ -78,9 +78,11 @@ def lorenz_animation(N_trajectories=20, rseed=1, frames=200, interval=30, rho_st
         ax.view_init(30, 0.3 * i)
         fig.canvas.draw()
         return lines + pts
-                
-    return animation.FuncAnimation(fig, animate, init_func=init,
-                                   frames=frames, interval=interval, blit = True)
-                                   
+
+    # Create animation and return the HTML for embedding
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=frames, interval=interval, blit=True)
+    return HTML(anim.to_jshtml())  # Use HTML for embedding animation in Jupyter Notebook
+
 
 
